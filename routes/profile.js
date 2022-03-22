4a617759af7e190f4510f50153d60e291382b384
sijadkhan1678 <sijadkhan1678@gmail.com>
2022-03-22 21:25:46 +0500

    created myprofile endpoint

--- routes/profile.js
+++ routes/profile.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router  = express.Router();
 const Profile = require('../models/Profile');
 const Article = require('../models/Article');
+const auth    = require('../middleware/auth')
 
 
-// *********** Method :   GET
-// *********** Route : /api/profile
-// *********** Disc  :  get user profile
-// *********** access : public
+  // *********** Method : GET
+  // *********** Route :  api/profile
+  // *********** Disc  :  get user profile
+  // *********** access : public
 
 
 router.get('/',async (req,res)=>{
@@ -15,7 +16,8 @@ router.get('/',async (req,res)=>{
       const userId= req.params.id
       try {
       const profile= await Profile.find({user: userId}).populate('user','photo name');
-      const articles = await Article.find({user: userId}).populate('auther','photo name');
+      
+      const articles = await Article.find({author: userId}).populate('author','photo name');
       
       res.json(profile,articles);
       
@@ -29,4 +31,34 @@ router.get('/',async (req,res)=>{
       }
 
 })
+
+  // ***********  Method :  GET
+  // ***********  Route :   api/profile/myprofile
+  // ***********  Disc  :   logged in user access personal profile
+  // ***********  access :  private
+
+
+    router.get('/myprofile',auth, async (req,res)=>{
+    
+    const userId= req.user.id
+    
+          try{
+          
+            const profile= await Profile({user:userId}).populate('user','photo name');
+            const articles= await Article.find({auther: userId}).populate('author').populate('author','photo name');
+            
+            res.json({profile,articles});
+        
+          }
+          catch(err){
+          
+          console.error(err.messge)
+          
+          res.status(500).send('server error')
+          }
+    
+    
+    })
+
+
 
